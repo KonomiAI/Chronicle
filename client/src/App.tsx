24c590859bacbc680ca8ab09535f5229068add51
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-02-03 17:44:18 -0500

    feat: added all the base files

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h1">Hello World</Typography>
+      <Button variant="contained">Click to impress</Button>
+    </Container>
+  );
+}
+
+export default App;
---
+++ client/src/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Inter',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+});
+
+export default theme;
