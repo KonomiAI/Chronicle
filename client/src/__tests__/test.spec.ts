32c389f392b81e4c95d2da9ba4bde339d1a464d4
Yonglin <wangyonglin1999@gmail.com>
2022-02-05 15:59:33 -0500

    feat: added base files for front-end project (#22)
    * feat: added all the base files
    
    * chore: added eslint and prettier config

---
+++ client/src/__tests__/test.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test, describe } from 'vitest';
+
+describe('basic test', () => {
+  test('will do math', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
---
+++ client/src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
+import App from './App';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
