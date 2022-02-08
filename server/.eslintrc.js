432b90762b55eaa5c53ea0b8076eb7af32f583c2
AnthonyLaiRH <lai.anthony.rh@gmail.com>
2022-02-07 20:47:46 -0500

    add airbnb to eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'airbnb',
     'plugin:prettier/recommended',
   ],
   root: true,
