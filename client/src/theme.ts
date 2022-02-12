b10a5b15e6aa539acf151103a1662f42c05a3ff1
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-02-12 13:14:18 -0500

    styling: improve card styling + theming

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,6 +1,16 @@
 import { createTheme } from '@mui/material';
+import { Shadows } from '@mui/material/styles/shadows';
 
 const theme = createTheme({
+  shadows: [
+    'none',
+    '0 0.125em 0.313em rgba(50,50,93,.09), 0 0.063em 0.125em rgba(0,0,0,.07)',
+    '0 0.250em 0.375em rgba(50,50,93,.09), 0 0.063em 0.188em rgba(0,0,0,.08)',
+    '0 0.063em 0.313em 0 rgba(0,0,0,.07), 0 0.438em 1.063em 0 rgba(0,0,0,.1)',
+    '0 0.938em 2.188em rgba(50,50,93,.1), 0 0.313em 0.938em rgba(0,0,0,.07)',
+    '0 0.938em 2.188em rgba(50,50,93,.15), 0 0.313em 0.938em rgba(0,0,0,.1)',
+    ...Array(19).fill('none'),
+  ] as Shadows,
   palette: {
     primary: {
       main: '#FF6961',
@@ -52,16 +62,30 @@ const theme = createTheme({
       fontWeight: 700,
     },
   },
+});
+
+/**
+ * This one adds component customizations while allowing
+ * us to use properties in the main theme.
+ */
+const finalTheme = createTheme(theme, {
   components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          boxShadow: theme.shadows[3],
+        },
+      },
+    },
     MuiAppBar: {
       styleOverrides: {
         root: {
           boxShadow: 'none',
-          borderBottom: `1px solid rgba(0, 0, 0, 0.12)`,
+          borderBottom: `1px solid ${theme.palette.divider}`,
         },
       },
     },
   },
 });
 
-export default theme;
+export default finalTheme;
