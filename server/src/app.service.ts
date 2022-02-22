e83511b2c3abcd448ecca6ccf0667f2107e68844
Daniel Wu <wuon@protonmail.com>
2022-02-22 14:02:22 -0500

    WIP

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  async getTest(): Promise<any> {
+    await prisma.$connect()
+    const allUsers = await prisma.post.findMany();
+    await prisma.$disconnect()
+    return allUsers;
+  }
+
+  async getTest2(): Promise<any> {
+    await prisma.$connect()
+    await prisma.post.create({
+      data: {
+        slug: 'test',
+        title: 'test',
+        body: 'test',
+      }
+    });
+    const allUsers = await prisma.post.findMany();
+    await prisma.$disconnect()
+    return allUsers;
   }
 }
