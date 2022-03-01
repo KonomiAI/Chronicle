d558b05a577428bd54252498acf8d3df950f714d
Andrew Pratheepan <andrew.pratheepan@gmail.com>
2022-03-01 17:21:58 -0500

    feat: added feature route and db seed

---
+++ server/prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main() {
+    const permissions = ['Product', 'Activity', 'Staff', 'Customer', 'Entry', 'Form']
+    for (var permission of permissions) {
+      const upsertPermissions = await prisma.feature.upsert({
+        where: { name: permission },
+        update: {},
+        create: {
+          name: permission
+        }
+      })
+      console.log(upsertPermissions)
+    }
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
---
+++ server/src/models/features/feature.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { FeatureController } from './feature.controller';
+import { FeatureService } from './feature.service';
+import { PrismaService } from '../../prisma.service';
+
+@Module({
+  controllers: [FeatureController],
+  providers: [FeatureService, PrismaService],
+})
+
+export class FeatureModule {}
