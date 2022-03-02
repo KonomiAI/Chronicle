71947eacd2065f653ed1b325904bceb841062722
Andrew Pratheepan <31967474+andrewpratheepan@users.noreply.github.com>
2022-03-01 20:43:36 -0500

    fix: update features in seed
    Co-authored-by: Yonglin <wangyonglin1999@gmail.com>

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const permissions = ['Product', 'Activity', 'Staff', 'Customer', 'Entry', 'Form']
+    const permissions = ['Inventory', 'Security', 'Customer', 'Entry', 'Form']
     for (var permission of permissions) {
       const upsertPermissions = await prisma.feature.upsert({
         where: { name: permission },
