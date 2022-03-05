9308f9634d9e8f3e0d0ca72bdbf10a5df5acd89c
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-03-05 11:51:13 -0500

    fix: added role seed and fix some issues

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -12,6 +12,61 @@ async function main() {
       },
     });
   }
+
+  await prisma.role.createMany({
+    data: [
+      {
+        name: 'Admin',
+        permissions: {
+          Inventory: {
+            read: true,
+            write: true,
+          },
+          Security: {
+            read: true,
+            write: true,
+          },
+          Customer: {
+            read: true,
+            write: true,
+          },
+          Entry: {
+            read: true,
+            write: true,
+          },
+          Form: {
+            read: true,
+            write: true,
+          },
+        },
+      },
+      {
+        name: 'Receptionist',
+        permissions: {
+          Inventory: {
+            read: true,
+            write: false,
+          },
+          Security: {
+            read: false,
+            write: false,
+          },
+          Customer: {
+            read: true,
+            write: true,
+          },
+          Entry: {
+            read: true,
+            write: false,
+          },
+          Form: {
+            read: true,
+            write: false,
+          },
+        },
+      },
+    ],
+  });
 }
 
 main()
