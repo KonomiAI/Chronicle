13712592b3024fcb36fa5e260008be57bb352195
Andrew Pratheepan <andrew.pratheepan@gmail.com>
2022-03-05 17:38:33 -0500

    fix: updated verify password and formatting issues

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -5,44 +5,45 @@ import { JwtService } from '@nestjs/jwt';
 
 //TODO: return roles associated as well when that relation has been established
 const SELECT = {
-    id: true,
-    firstName: true,
-    lastName: true,
-    email: true,
-    authKey: true,
-    gender: true,
-    isSuperUser: true,
+	id: true,
+	firstName: true,
+	lastName: true,
+	email: true,
+	authKey: true,
+	gender: true,
+	isSuperUser: true,
 }
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private staffService: StaffService,
-        private jwtService: JwtService,
-        private bcrypt: BcryptService
-    ) { }
-    async validateUser(email: string, password: string) {
-        const user = await this.getDetails({ email }, SELECT)
-        if (user?.authKey && this.verifyPassword(password, user.authKey)) {
-            const { authKey, ...result } = user;
-            return result;
-        }
-        throw new BadRequestException(
-            'User with the username and password is not found',
-        );
-    }
-    private verifyPassword(pass: string, hash: string) {
-        return this.bcrypt.verify(pass, hash);
-    }
-    async obtainToken(user: any) {
-        const payload = { email: user.email, sub: user.id };
-        return {
-            accessToken: this.jwtService.sign(payload),
-            ...user,
-        };
-    }
+	constructor(
+		private staffService: StaffService,
+		private jwtService: JwtService,
+		private bcrypt: BcryptService
+	) { }
+	async validateUser(email: string, password: string) {
+		const user = await this.getDetails({ email }, SELECT)
+		const valid = await this.verifyPassword(password, user.authKey)
+		if (user?.authKey && valid) {
+			const { authKey, ...result } = user;
+			return result;
+		}
+		throw new BadRequestException(
+			'User with the username and password is not found',
+		);
+	}
+	private verifyPassword(pass: string, hash: string) {
+		return this.bcrypt.verify(pass, hash);
+	}
+	async obtainToken(user: any) {
+		const payload = { email: user.email, sub: user.id };
+		return {
+			accessToken: this.jwtService.sign(payload),
+			...user,
+		};
+	}
 
-    async getDetails(payload, select) {
-        return await this.staffService.findOne(payload, select)
-    }
+	getDetails(payload, select) {
+		return this.staffService.findOne(payload, select)
+	}
 }
--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -5,26 +5,26 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 const SELECT = {
-    id: true,
-    email: true, 
-    isSuperUser: true,
-    isSuspended: true
+	id: true,
+	email: true,
+	isSuperUser: true,
+	isSuspended: true
 }
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
-            secretOrKey: jwtConstants.secret,
-        });
-    }
-    async validate(payload: any) {
-        const user = await this.authService.getDetails({id: payload.sub}, SELECT)
-        if (!user) {
-            throw new UnauthorizedException();
-        }
-        return user;
-    }
+	constructor(private authService: AuthService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: false,
+			secretOrKey: jwtConstants.secret,
+		});
+	}
+	async validate(payload: any) {
+		const user = await this.authService.getDetails({ id: payload.sub }, SELECT)
+		if (!user) {
+			throw new UnauthorizedException();
+		}
+		return user;
+	}
 }
