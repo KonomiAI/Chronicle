def0293b338fc80b6e3199343322c7d78564d939
Patrick Nguyen <patrickonguyen@gmail.com>
2022-03-07 09:37:05 -0500

    update: use PageHeader component

--- client/src/pages/allowlist/index.tsx
+++ client/src/pages/allowlist/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@mui/material';
 
 import DeleteIcon from '@mui/icons-material/Delete';
+import PageHeader from '../../components/page-header/PageHeader';
 
 const ipList = [
   { ip: "192.168.0.1", description: "Some description about this IP address" },
@@ -43,31 +44,25 @@ const AllowListRow = () => (
 export default function AllowListPage() {
   return (
     <Container maxWidth="sm">
-      <Box
+      <Box  
         sx={{
           marginTop: '3em',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'space-between',
           mb: '1em',
-          gap: '2em',
+          gap: '5em',
         }}
       >
-        <Box>
-          <Typography variant="h2">IP Allowlist</Typography>
-          <Typography variant="body2">
-            Add IP address to the allowlist to ensure your staff member can only access
-            the application from certain locations
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            whiteSpace: 'nowrap',
-          }}
-        >
-          <Button variant="contained">Add New</Button>
-        </Box>
-      </Box>
+        <PageHeader 
+          pageTitle="IP Allowlist"
+          helpText="Add IP address to the allowlist to ensure your staff member can only access
+          the application from certain locations"
+          action={
+            <Button variant="contained">Add New</Button>
+          }
+        />
+      </Box>   
       <TableContainer component={Paper}>
         <Table>
           <TableBody>
