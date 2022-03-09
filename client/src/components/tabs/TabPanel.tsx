2488f0aad83e3d97ebde04a5ab6cc5ae0c186b60
Patrick Nguyen <patrickonguyen@gmail.com>
2022-03-08 22:59:54 -0500

    feat: tab panel component

---
+++ client/src/components/tabs/TabPanel.tsx
@@ -0,0 +1,28 @@
+import {
+    Box,
+    Typography,
+  } from '@mui/material';
+
+
+export interface TabPanelProps {
+    children: any;
+    index: number;
+    value: number;
+}
+
+export default function TabPanel(props:TabPanelProps) {
+const { children, value, index, ...other } = props;
+
+return (
+    <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-auto-tabpanel-${index}`}
+        aria-labelledby={`scrollable-auto-tab-${index}`}
+        {...other}
+    >
+        <Box p={3}>{children}</Box>
+    </Typography>
+);
+}
