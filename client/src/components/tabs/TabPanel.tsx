1e29380ccf81583b4a7ee8f4a080a347b5c53b73
Patrick Nguyen <patrickonguyen@gmail.com>
2022-03-16 20:01:25 -0400

    fix: use prettier

--- client/src/components/tabs/TabPanel.tsx
+++ client/src/components/tabs/TabPanel.tsx
@@ -1,28 +1,24 @@
-import {
-    Box,
-    Typography,
-  } from '@mui/material';
-
+import { Box, Typography } from '@mui/material';
 
 export interface TabPanelProps {
-    children: any;
-    index: number;
-    value: number;
+  children: any;
+  index: number;
+  value: number;
 }
 
-export default function TabPanel(props:TabPanelProps) {
-const { children, value, index, ...other } = props;
+export default function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-return (
+  return (
     <Typography
-        component="div"
-        role="tabpanel"
-        hidden={value !== index}
-        id={`scrollable-auto-tabpanel-${index}`}
-        aria-labelledby={`scrollable-auto-tab-${index}`}
-        {...other}
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
     >
-        <Box p={3}>{children}</Box>
+      <Box p={3}>{children}</Box>
     </Typography>
-);
+  );
 }
