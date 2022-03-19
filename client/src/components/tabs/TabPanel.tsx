a8799c9e723ed97d65d37356ff5f65a45622fb6f
Patrick Nguyen <patrickonguyen@gmail.com>
2022-03-19 16:45:46 -0400

    feat: Inventory UI First Pass (#110)
    * feat: simple inventory list
    
    * feat: inventory edit dangerzone
    
    * feat: inventory edit page product
    
    * feat: inventory edit page variants table
    
    * feat: inventory create ui with variant dialog
    
    * feat: inventory create add dropzone
    
    * dependecy: dropzone-ui
    
    * feat: add inventory routing
    
    * feat: add default values for inventory edit
    
    * feat: inventory page variant list
    
    * feat: inventory list page table tab view
    
    * feat: tab panel component
    
    * fix: dependencies
    
    * fix: inventory edit path
    
    * feat: divide inventory table into a component
    
    * feat: seperate inventory table component
    
    * fix: use prettier
    
    * fix: use prettier
    
    * fix: props type
    
    * fix: remove props spreading
    
    * feat: remove props spreading
    
    * fix: prettier
    
    Co-authored-by: Patrick Nguyen <panguyen@blizzard.com>

---
+++ client/src/components/tabs/TabPanel.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export interface TabPanelProps {
+  children: any;
+  index: number;
+  value: number;
+}
+
+export default function TabPanel(props: TabPanelProps) {
+  const { children, value, index } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
