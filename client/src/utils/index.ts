fb622a63dfe2095a9ef9579d7f73ee009ebbceef
Yonglin <wangyonglin1999@gmail.com>
2022-03-25 14:19:29 -0400

    feat: added login integration (#125)
    * feat: added login integration
    
    * fix: added loading and simple error
    
    Co-authored-by: Andrew Pratheepan <31967474+andrewpratheepan@users.noreply.github.com>
    Co-authored-by: Daniel Wu <wuon@protonmail.com>

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,6 @@
 export * from './random';
 export * from './validation';
 export * from './clipboard/use-clipboard';
+export * from './storage';
+export * from './storage-helpers';
+export * from './auth';
