2ebafa5d111958dfff8db5e12d6dd260dd63ca08
Andrew Pratheepan <31967474+andrewpratheepan@users.noreply.github.com>
2022-03-30 22:08:15 -0400

    feat: save bar resizes with the app drawer (#129)
    * feat: save bar resizes with the app drawer
    
    * fix: updated store state typing
    
    * fix: prettier
    
    * fix: missed  when dealing with merge

--- client/src/components/save-bar/save-bar.tsx
+++ client/src/components/save-bar/save-bar.tsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import {
-  AppBar,
-  Button,
-  Container,
-  Grid,
-  Toolbar,
-  Typography,
-} from '@mui/material';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Container, Grid, Toolbar, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { DRAWER_WIDTH } from '../../vars';
+import { useStore } from '../../store';
 
+interface AppBarProps extends MuiAppBarProps {
+  open: boolean;
+}
 export interface SaveBarProps {
   open: boolean;
   loading?: boolean;
   onSave: () => void;
 }
 
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: DRAWER_WIDTH,
+    width: `calc(100% - ${DRAWER_WIDTH}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
 export default function SaveBar({ onSave, open, loading }: SaveBarProps) {
   const [isOpen, setIsOpen] = useState(open);
   const [isLoading] = useState(!!loading);
+  const minState = useStore.getState().sidebarOpen;
+  const [sidebarOpen, setSideBar] = useState(minState);
 
   useEffect(() => {
     setIsOpen(open);
+    useStore.subscribe(
+      (state) => state.sidebarOpen,
+      (state) => setSideBar(state),
+    );
   }, [open]);
-
   return (
     <AppBar
       position="fixed"
       color="inherit"
       sx={{ top: 'auto', bottom: 0, display: isOpen ? 'block' : 'none' }}
+      open={sidebarOpen}
     >
       <Toolbar>
         <Container>
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,16 +1,28 @@
-import create, { GetState, State } from 'zustand';
-import { devtools, NamedSet } from 'zustand/middleware';
+import create, { GetState, SetState, Mutate, StoreApi } from 'zustand';
+import { devtools, NamedSet, subscribeWithSelector } from 'zustand/middleware';
+
+interface IuiState {
+  sidebarOpen: boolean;
+}
 
 // Allows for cross-slice access
-export type StoreState = State;
+export type StoreState = IuiState;
 
 export type StoreSlice<T> = (
   set: NamedSet<StoreState>,
   get: GetState<StoreState>,
 ) => T;
 
-export const useStore = create<StoreState>(
-  devtools((set, get) => ({
-    // Slices
-  })),
+export const useStore = create<
+  StoreState,
+  SetState<StoreState>,
+  GetState<StoreState>,
+  Mutate<
+    StoreApi<StoreState>,
+    [['zustand/subscribeWithSelector', never], ['zustand/devtools', never]]
+  >
+>(
+  devtools(
+    subscribeWithSelector((set, get): StoreState => ({ sidebarOpen: true })),
+  ),
 );
