d1d666897239ad42bc9e520a0e74861ae752d6ad
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-10-19 09:13:42 -0400

    feat: added customer gender handling

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,5 @@
 import SaveBar from './save-bar/save-bar';
+import GenderSelect from './gender-select/GenderSelect';
 
 export * from './save-bar/save-bar';
 export * from './form-builder/form-builder';
@@ -10,4 +11,4 @@ export * from './variant-card/variant-card';
 export * from './customer-card/customer-card';
 
 /** Add all default exported components to this folder */
-export { SaveBar };
+export { SaveBar, GenderSelect };
