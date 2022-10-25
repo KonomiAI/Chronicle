d9ca2ff63095b8feb688581bda8b9070a396e385
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-10-25 16:10:00 -0400

    fix: made custom form readonly

--- client/src/components/form-builder/FieldTypeSelect.tsx
+++ client/src/components/form-builder/FieldTypeSelect.tsx
@@ -9,6 +9,7 @@ import {
 } from '@mui/material';
 import { Control, Controller } from 'react-hook-form';
 import { FormSupportedFieldTypes } from '@konomi.ai/c-form';
+import { usePermission } from '../use-permission/UsePermissionContext';
 
 export interface FieldTypeSelectProps {
   control: Control;
@@ -20,49 +21,53 @@ export const FieldTypeSelect = ({
   control,
   name,
   onChange,
-}: FieldTypeSelectProps) => (
-  <Controller
-    name={name}
-    control={control}
-    rules={{ required: true }}
-    render={({
-      field: { onChange: onControllerChange, value },
-      fieldState: { invalid },
-    }) => (
-      <FormControl fullWidth>
-        <InputLabel id="questionTypeLabel">Question type</InputLabel>
-        <Select
-          labelId="questionTypeLabel"
-          value={value}
-          size="small"
-          onChange={(e) => {
-            if (onChange) {
-              onChange(e);
-            }
-            onControllerChange(e);
-          }}
-          label="Question type"
-          error={invalid}
-        >
-          <Divider textAlign="left">Text</Divider>
-          <MenuItem value={FormSupportedFieldTypes.TEXT}>Short text</MenuItem>
-          <MenuItem value={FormSupportedFieldTypes.LONG_TEXT}>
-            Paragraph
-          </MenuItem>
-          <Divider textAlign="left">Choice</Divider>
-          <MenuItem value={FormSupportedFieldTypes.MULTIPLE_CHOICE}>
-            Multiple choice
-          </MenuItem>
-          <MenuItem value={FormSupportedFieldTypes.MULTI_SELECT}>
-            Multi-select
-          </MenuItem>
-          <MenuItem value={FormSupportedFieldTypes.DATA_SOURCE_SELECT}>
-            Dynamic data select
-          </MenuItem>
-          <Divider textAlign="left">Other</Divider>
-          <MenuItem value={FormSupportedFieldTypes.NUMBER}>Number</MenuItem>
-        </Select>
-      </FormControl>
-    )}
-  />
-);
+}: FieldTypeSelectProps) => {
+  const { canWrite } = usePermission();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: true }}
+      render={({
+        field: { onChange: onControllerChange, value },
+        fieldState: { invalid },
+      }) => (
+        <FormControl fullWidth>
+          <InputLabel id="questionTypeLabel">Question type</InputLabel>
+          <Select
+            labelId="questionTypeLabel"
+            value={value}
+            size="small"
+            disabled={!canWrite}
+            onChange={(e) => {
+              if (onChange) {
+                onChange(e);
+              }
+              onControllerChange(e);
+            }}
+            label="Question type"
+            error={invalid}
+          >
+            <Divider textAlign="left">Text</Divider>
+            <MenuItem value={FormSupportedFieldTypes.TEXT}>Short text</MenuItem>
+            <MenuItem value={FormSupportedFieldTypes.LONG_TEXT}>
+              Paragraph
+            </MenuItem>
+            <Divider textAlign="left">Choice</Divider>
+            <MenuItem value={FormSupportedFieldTypes.MULTIPLE_CHOICE}>
+              Multiple choice
+            </MenuItem>
+            <MenuItem value={FormSupportedFieldTypes.MULTI_SELECT}>
+              Multi-select
+            </MenuItem>
+            <MenuItem value={FormSupportedFieldTypes.DATA_SOURCE_SELECT}>
+              Dynamic data select
+            </MenuItem>
+            <Divider textAlign="left">Other</Divider>
+            <MenuItem value={FormSupportedFieldTypes.NUMBER}>Number</MenuItem>
+          </Select>
+        </FormControl>
+      )}
+    />
+  );
+};
