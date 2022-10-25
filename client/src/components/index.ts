1572174ea9f9a21dfc456dc670c00560beb17c7b
Yonglin <wangyonglin1999@gmail.com>
2022-10-24 21:34:26 -0400

    feat: made security features read-only (#319)
    * fix: made staff page read only
    
    * fix: made roles readonly
    
    * fix: made allowlist readonly

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 import SaveBar from './save-bar/save-bar';
 import GenderSelect from './gender-select/GenderSelect';
+import PermissionProvider from './use-permission/UsePermissionContext';
 
 export * from './save-bar/save-bar';
 export * from './form-builder/form-builder';
@@ -8,6 +9,7 @@ export * from './utils/util-components';
 export * from './activity-card/activity-card';
 export * from './variant-card/variant-card';
 export * from './customer-card/customer-card';
+export * from './use-permission/UsePermissionContext';
 
 /** Add all default exported components to this folder */
-export { SaveBar, GenderSelect };
+export { SaveBar, GenderSelect, PermissionProvider };
