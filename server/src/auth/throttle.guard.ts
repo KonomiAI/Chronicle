c0564fb73a9b1a2cf3ca63bddec8374822a7e4bd
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-10-30 15:58:03 -0400

    fix: disable throttle on develop

--- server/src/auth/throttle.guard.ts
+++ server/src/auth/throttle.guard.ts
@@ -1,10 +1,16 @@
 // throttler-behind-proxy.guard.ts
 import { ThrottlerGuard } from '@nestjs/throttler';
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { getIPFromRequest } from './ip.guard';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
+  canActivate(context: ExecutionContext): Promise<boolean> {
+    if (process.env.CHRONICLE_DISABLED_THROTTLE === 'YES') {
+      return Promise.resolve(true);
+    }
+    return super.canActivate(context);
+  }
   protected getTracker(req: Request): string {
     return getIPFromRequest(req);
   }
